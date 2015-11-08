013548410b0618e6055b99aa36080e8582c201a4
Prakal <pranav.89@gmail.com>
2015-11-07 20:39:25 -0800

    frontend changes - date picker with return post request

--- public/app/events/events.js
+++ public/app/events/events.js
@@ -1,12 +1,41 @@
-angular.module('volare.events', [])
+angular.module('volare.events', ['ngMaterial'])
 
 .controller('EventsController', function ($scope, $http, $window, $timeout, $q) {
-    console.log('something happend');
-
-  // Stores all events that were created by you or that you were invited to
-  $scope.user = {
-      firstName: '',
-      lastName: '' ,
+    console.log('something happened');
+    $scope.startDate = new Date();
+    $scope.endDate = new Date();
+    $scope.minDate = new Date(
+    $scope.startDate.getFullYear(),
+    $scope.startDate.getMonth(),
+    $scope.startDate.getDate());
+    $scope.maxDate = new Date(
+    $scope.endDate.getFullYear(),
+    $scope.endDate.getMonth() + 2,
+    $scope.endDate.getDate());
+    console.log($scope.startDate,$scope.endDate);
+    // Stores all events that were created by you or that you were invited to
+    $scope.user = {
+        firstName: '',
+        lastName: '' ,
     };
+
+    Date.prototype.yyyymmdd = function() {
+       var yyyy = this.getFullYear().toString();
+       var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+       var dd  = this.getDate().toString();
+       return yyyy + '-' +(mm[1]?mm:"0"+mm[0]) + '-' +(dd[1]?dd:"0"+dd[0]); // padding
+      };
+    $scope.search = function(event){
+      console.log('seaRch triggered', $window.ourStartDate, $window.ourEndDate);
+      $http.post('/someUrl', {'startDate':$window.ourStartDate.yyyymmdd(),'endDate':$window.ourEndDate.yyyymmdd()}).then(function(){successCallback}, function(){errorCallback});
+    };
+    $scope.setNewDate = function(){
+      $window.ourStartDate = $scope.startDate;
+      $window.ourEndDate = $scope.endDate;
+
+      console.log($window.ourStartDate,$window.ourEndDate);
+    };
+
   }
 );
+
