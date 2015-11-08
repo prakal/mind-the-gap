7bda589b86dcd85de0b0ddca75378aa1a08b1c43
Anna Zhu <zhuanna93@gmail.com>
2015-11-07 16:18:29 -0800

    Added flights.js to new Data folder

---
+++ server/Data/flights.js
@@ -0,0 +1,71 @@
+{ 
+  flights: [
+            { origin: 'SFO',
+              dest: 'JFK',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '300' },
+
+            { origin: 'SFO',
+              dest: 'LAS',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '200' },
+
+            { origin: 'SFO',
+              dest: 'SYD',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '1500' },
+
+            { origin: 'SFO',
+              dest: 'TYO',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '2000' },
+
+            { origin: 'SFO',
+              dest: 'MEX',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '900' },
+
+            { origin: 'SFO',
+              dest: 'DXB',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '1000' },
+
+            { origin: 'SFO',
+              dest: 'LON',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '1500' },
+
+            { origin: 'SFO',
+              dest: 'LAX',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '150' },
+
+            { origin: 'SFO',
+              dest: 'TXL',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '1500' },
+
+            { origin: 'SFO',
+              dest: 'CDG',
+              departureTime: '2015-12-24T21:00:00',
+              arrivalTime: '2015-12-24T21:00:00',
+              price: '2000' },
+ ] 
+}
+/*
+new york(JFK), las vegas(LAS), 
+sydney(SYD), tokyo(TYO), 
+mexico city(MEX), dubai(DXB), 
+london(LON), los angeles(LAX), 
+berlin(TXL), paris(CDG)
+
+*/
